7e96c675cc810a2c3b9713246986c2db9c82dbc3
ujangfatahak <63782886+ujangfatahak@users.noreply.github.com>
2020-04-17 19:46:34 +0700

    menambah database

---
+++ Praktikum/p5_PW_193040174/Latihan5a/index.php
@@ -0,0 +1,56 @@
+<?php
+//melakukan koneksi ke database
+$conn = mysqli_connect("localhost", "root", "") or die("koneksi ke DB gagal");
+
+//memilih database
+mysqli_select_db($conn, "pw_193040174") or die("Database salah!");
+
+//query mengambil objek dari tabel kedalam database
+$result = mysqli_query($conn, "SELECT * FROM alat musik");
+?>
+<html>
+    <head>
+        <title>Latihan5a_193040174</title>
+    </head>
+    <style>
+		tr {
+			font-size: 20px;
+			text-align: center;
+		}
+		img {
+			width: 250px;
+		}
+		h3 {
+			text-align: center;
+			font-size: 30px;
+		}
+    </style>
+    <body>
+        <div class="container">
+        <table border="1" cellspacing="0" cellpadding="10">
+            <tr bgcolor="lightgrey">
+                <th>NO</th>
+                <th>GAMBAR</th>
+                <th>NAMA</th>
+                <th>DESKRIPSI</th>
+                <th>ASAL DAERAH</th>
+                <th>CARA PEMAKAIAN</th>
+                <th>HARGA</th>
+            </tr>
+            <?php $i = 1 ?>
+            <?php while($row = mysqli_fetch_assoc($result)) :  ?>
+            <tr>
+                <td><?= $i ?></td>
+                <td><img src="assets/img/<?= $row['foto']; ?>"></td>
+                <td><?= $row["nama"]; ?></td>
+                <td><?= $row["deskripsi"]; ?></td>
+                <td><?= $row["asal daerah"]; ?></td>
+                <td><?= $row["cara pemakaian"]; ?></td>
+                <td><?= $row["harga"]; ?></td>
+            </tr>
+            <?php $i++  ?>
+            <?php endwhile; ?>
+        </table>
+        </div>
+    </body>
+</html>
